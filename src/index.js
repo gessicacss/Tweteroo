997125be8a1722ea5f5f352cd257a1c5ee3adc9a
Gessica <santos.gessicacarolina@gmail.com>
2023-04-05 14:05:45 -0300

    request GET tweets (fixed array of tweets)

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const server = express();
 server.use(cors());
 
 server.get('/tweets', (req, res) => {
-    const tweet = [
+    const tweets = [
         {
             username: "bobesponja",
             avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
             tweet: "Eu amo hambúrguer de siri!"
+        }, 
+        {
+            username: "oi",
+            avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+            tweet: "oie!"
         }
     ]
-    res.send(tweet);
+    res.send(tweets);
 })
 
-server.listen(5000, () => console.log('running on port 5000'))
+const port = 5000;
+server.listen(port, () => console.log(`running on port ${port}`));
