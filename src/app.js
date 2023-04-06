1e262966bbbd9d996c99f31a74df120b595f1abc
Gessica <santos.gessicacarolina@gmail.com>
2023-04-05 22:05:53 -0300

    send tweets request done

--- src/app.js
+++ src/app.js
@@ -15,6 +15,20 @@ server.post('/sign-up', (req, res) => {
   res.status(201).send('OK');
 })
 
+
+server.post('/tweets', (req, res) => {
+  const {username, tweet} = req.body;
+  const findUser = users.find((u) => u.username === username);
+  if (!findUser){
+    res.status(401).send('UNAUTHORIZED');
+    return
+  }
+  const avatar = findUser.avatar;
+  const newTweet = {username, tweet, avatar};
+  tweets.push(newTweet);
+  res.status(201).send('OK');
+})
+
 server.get("/tweets", (req, res) => {
   res.send(tweets);
 });
